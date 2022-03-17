d9223d6ccba744a8a02171f7e927d885f9c0e386
kaihulme <kaihulme97@gmail.com>
2022-03-17 18:10:46 +0000

    Require login for all views

--- nestpi/auth.py
+++ nestpi/auth.py
@@ -8,6 +8,16 @@ from db import get_db
 bp = Blueprint("auth", __name__, url_prefix="/auth")
 
 
+def login_required(view):
+    @functools.wraps(view)
+    def wrapped_view(**kwargs):
+        if g.user is None:
+            return redirect(url_for("auth.login"))
+        return view(**kwargs)
+
+    return wrapped_view
+
+
 @bp.route("/login", methods=("GET", "POST"))
 def login():
 
