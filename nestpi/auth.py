0744e3b06ef2234cca88a5e28de564baafe4ebf4
kaihulme <kaihulme97@gmail.com>
2022-03-17 18:09:39 +0000

    Login / logout

--- nestpi/auth.py
+++ nestpi/auth.py
@@ -8,6 +8,40 @@ from db import get_db
 bp = Blueprint("auth", __name__, url_prefix="/auth")
 
 
+@bp.route("/login", methods=("GET", "POST"))
+def login():
+
+    if request.method == "POST":
+
+        username = request.form["username"]
+        password = request.form["password"]
+        db = get_db()
+        error = None
+        user = db.execute(
+            "SELECT * FROM user WHERE username = ?", (username,)
+        ).fetchone()
+
+        if user is None:
+            error = "Incorrect username."
+        elif not check_password_hash(user["password"], password):
+            error = "Incorrect password."
+
+        if error is None:
+            session.clear()
+            session["user_id"] = user["id"]
+            return redirect(url_for("index"))
+
+        flash(error)
+
+    return render_template("auth/login.html")
+
+
+@bp.route("/logout")
+def logout():
+    session.clear()
+    return redirect(url_for("index"))
+
+
 @bp.route("/register", methods=("GET", "POST"))
 def register():
 
