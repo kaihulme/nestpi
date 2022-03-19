77ccc9531c0863d3b419164fc853c40a2c75a0e9
kaihulme <kaihulme97@gmail.com>
2022-03-19 10:59:50 +0000

    Add factory test coverage

--- tests/test_factory.py
+++ tests/test_factory.py
@@ -2,5 +2,25 @@ from nestpi import create_app
 
 
 def test_config():
+    """
+    Check app is run in testing configuration.
+    """
     assert not create_app().testing
     assert create_app({"TESTING": True}).testing
+
+
+def test_hello(client):
+    """
+    Check reponse of /hello.
+    """
+    response = client.get("/hello/")
+    assert b"Hello World!" in response.data
+
+
+def test_index(client):
+    """
+    Check index/ redirects to hello/.
+    """
+    response = client.get("/index/", follow_redirects=True)
+    assert len(response.history) == 1
+    assert response.request.path == "/hello/"
