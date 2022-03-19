3ca90d3d27b02ab88811cedf179c00676575b239
kaihulme <kaihulme97@gmail.com>
2022-03-19 14:02:19 +0000

    Fix login persistance

--- nestpi/auth.py
+++ nestpi/auth.py
@@ -46,6 +46,17 @@ def login():
     return render_template("auth/login.html")
 
 
+@bp.before_app_request
+def load_logged_in_user():
+    user_id = session.get("user_id")
+    if user_id:
+        g.user = (
+            get_db().execute("SELECT * FROM user WHERE id = ?", (user_id,)).fetchone()
+        )
+    else:
+        g.user = None
+
+
 @bp.route("/logout")
 def logout():
     session.clear()
