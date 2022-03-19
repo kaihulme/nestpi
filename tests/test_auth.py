f5c5445eb7652244fecc200a3199466aba126fc9
kaihulme <kaihulme97@gmail.com>
2022-03-19 15:22:49 +0000

    Authentication test coverage

--- tests/test_auth.py
+++ tests/test_auth.py
@@ -1,9 +1,50 @@
+import pytest
 from flask import session, g
+
+from nestpi.db import get_db
 
 
 def test_login(client, auth):
-    auth.login()
     with client:
+        auth.login()
         client.get("/")
         assert session["user_id"]
         assert g.user["username"] == "test"
+
+
+def test_logout(client, auth):
+    with client:
+        auth.login()
+        auth.logout()
+        assert "user_id" not in session
+
+
+@pytest.mark.parametrize(
+    ("username", "password", "message"),
+    (("x", "test", b"Incorrect username"), ("test", "x", b"Incorrect password")),
+)
+def test_invalid_login(client, auth, username, password, message):
+    assert message in auth.login(username, password).data
+
+
+def test_register(app, client):
+    client.post("/auth/register", data={"username": "x", "password": "x"})
+    with app.app_context():
+        assert get_db().execute("SELECT * FROM user WHERE username = 'x'").fetchone()
+
+
+@pytest.mark.parametrize(
+    ("username", "password", "message"),
+    (
+        ("x", "", b"Password is required"),
+        ("", "x", b"Username is required"),
+        ("test", "test", b"User test is already registered"),
+    ),
+)
+def test_invalid_register(client, username, password, message):
+    assert (
+        message
+        in client.post(
+            "/auth/register", data={"username": username, "password": password}
+        ).data
+    )
