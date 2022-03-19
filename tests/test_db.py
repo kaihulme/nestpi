25f4df0f2709d205c42eb56bd37509b6f170600d
kaihulme <kaihulme97@gmail.com>
2022-03-19 12:06:21 +0000

    Add database test coverage

--- tests/test_db.py
+++ tests/test_db.py
@@ -1,4 +1,16 @@
+import pytest
+import sqlite3
+
 from nestpi.db import get_db
+
+
+def test_db_connection(app):
+    """
+    Check app connects to database.
+    """
+    with app.app_context():
+        db = get_db()
+        db.execute("SELECT 1")
 
 
 def test_is_same_db(app):
@@ -8,3 +20,23 @@ def test_is_same_db(app):
     with app.app_context():
         db = get_db()
         assert db is get_db()
+
+
+def test_db_is_closed(app):
+    """
+    Check database is closed outside app context.
+    """
+    with app.app_context():
+        db = get_db()
+    with pytest.raises(sqlite3.ProgrammingError) as e:
+        db.execute("SELECT 1")
+    assert "closed" in str(e.value)
+
+
+def test_db_has_user_table(app):
+    """
+    Check database has users table.
+    """
+    with app.app_context():
+        db = get_db()
+        db.execute("SELECT * FROM user")
